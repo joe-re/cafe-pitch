59240f44173125df55d43c11479319f5ed8e0e2b
joe-re <joe.tialtngo@gmail.com>
2016-02-14 09:11:54 +0900

    refactor directory structure

---
+++ src/renderer/app.ts
@@ -0,0 +1,6 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './components/app.component';
+
+document.addEventListener('DOMContentLoaded', function() {
+  bootstrap(AppComponent);
+});
--- src/renderer/components/app.component.ts
+++ src/renderer/components/app.component.ts
@@ -1,14 +1,9 @@
 import {Component} from 'angular2/core';
 import {Editor} from './editor/editor.component';
-import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
   selector: 'my-app',
   template: '<editor></editor>',
   directives: [Editor]
 })
-class AppComponent { }
-
-document.addEventListener('DOMContentLoaded', function() {
-  bootstrap(AppComponent);
-});
+export class AppComponent { }
