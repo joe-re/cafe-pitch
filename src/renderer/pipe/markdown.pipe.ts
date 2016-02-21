6e3be90e35e574431938ab4129b9f0071893d508
joe-re <joe.tialtngo@gmail.com>
2016-02-22 05:05:01 +0900

    replace s/markey-markdown/marked/

--- src/renderer/pipe/markdown.pipe.ts
+++ src/renderer/pipe/markdown.pipe.ts
@@ -1,9 +1,9 @@
 import {Pipe, PipeTransform} from 'angular2/core';
-const marky = require('marky-markdown');
+const marked = require('marked');
 
 @Pipe({name: 'markdown'})
 export class MarkdownPipe implements PipeTransform {
   transform(value: string, args: string[]): any {
-    return marky(value).html();
+    return marked(value);
   }
 }
