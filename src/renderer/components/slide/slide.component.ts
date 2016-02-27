e7bc8a9836afb4fb4a31a9aecffd8dc77d3d5756
joe-re <joe.tialtngo@gmail.com>
2016-02-28 04:36:49 +0900

    deal with resizing window

--- src/renderer/components/slide/slide.component.ts
+++ src/renderer/components/slide/slide.component.ts
@@ -5,7 +5,9 @@ import {MarkdownPipe} from '../../pipe/markdown.pipe';
   selector: 'slide',
   template: `
     <div class="slide" (window:resize)="onResize($event)">
-     <div class="content" [innerHTML]="text | markdown"></div>
+     <div class="inner">
+       <div class="content" [innerHTML]="text | markdown"></div>
+     </div>
    </div>
   `,
   styles: [`
@@ -13,10 +15,14 @@ import {MarkdownPipe} from '../../pipe/markdown.pipe';
       width: 100%;
       height: 100%;
     }
-    .content {
+    .inner {
       height: 40vh;
       background: white;
       position: absolute;
+      text-align: center;
+    }
+    .content {
+      position: relative;
     }
     `
   ],
@@ -28,10 +34,22 @@ export class Slide {
   slideWidth: number;
   constructor(private el: ElementRef) { }
   onResize(e: MouseEvent) {
-    const content = this.el.nativeElement.querySelector('slide .content');
-    content.style.width = `${content.clientHeight * 16 / 9}px`;
+    const inner = this.el.nativeElement.querySelector('slide .inner');
+    inner.style.width = `${inner.clientHeight * 16 / 9}px`;
     const slide = this.el.nativeElement.querySelector('.slide');
-    content.style.top = `${(slide.clientHeight - content.clientHeight) / 2}px`;
-    content.style.left = `${(slide.clientWidth - content.clientWidth) / 2}px`;
+    inner.style.top = `${(slide.clientHeight - inner.clientHeight) / 2}px`;
+    inner.style.left = `${(slide.clientWidth - inner.clientWidth) / 2}px`;
+    this.setContentTop();
+  }
+  ngAfterViewChecked() {
+    this.setContentTop();
+  }
+  setContentTop() {
+    const inner = this.el.nativeElement.querySelector('slide .inner');
+    const content = this.el.nativeElement.querySelector('slide .content');
+    const contentTop = (inner.clientHeight - content.clientHeight) / 2;
+    if (contentTop > 0) {
+      content.style.top = `${contentTop}px`;
+    }
   }
 }
