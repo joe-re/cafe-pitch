77812f62a38373d5e02c41f7dbb7d70137e5c7b6
joe-re <joe.tialtngo@gmail.com>
2016-04-13 09:29:41 +0900

    remove angular-polifulls from script tag
    If required library is imported on js, so angular-polifulls is unnecessary.

--- src/renderer/export.ts
+++ src/renderer/export.ts
@@ -1,3 +1,7 @@
+import 'es6-shim';
+import 'reflect-metadata';
+import 'rxjs/Rx';
+import 'zone.js/dist/zone';
 import {bootstrap} from 'angular2/platform/browser';
 import {ExportComponent} from './components/export.component';
 
