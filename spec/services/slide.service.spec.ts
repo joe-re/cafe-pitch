ee4ee16641415e0cbc04d2cdde340f213e167a6d
joe-re <joe.tialtngo@gmail.com>
2016-05-03 21:21:30 +0900

    write spec of slide service

---
+++ spec/services/slide.service.spec.ts
@@ -0,0 +1,77 @@
+import { describe, it, inject, expect, beforeEachProviders, beforeEach } from 'angular2/testing';
+import { SlideService } from '../../src/renderer/services/slide.service';
+
+describe('SlideService', () => {
+
+    beforeEachProviders(() => [
+        SlideService
+    ]);
+
+    it('Should service exist', inject([SlideService], (testee: SlideService) => {
+        expect(testee).toBeDefined();
+    }));
+
+    describe('#getPageText', () => {
+      let testee: SlideService;
+      beforeEach(inject([SlideService], (s: SlideService) => testee = s));
+
+      describe('text is nothing', () => {
+        it('should be empty', () => {
+          expect(testee.getPageText(1)).toEqual('');
+        });
+      });
+
+      describe('text has been setted', () => {
+        beforeEach(() => {
+          testee.setText('# page1\n---\n# page2');
+        });
+        it('should get text by page', () => {
+          expect(testee.getPageText(1)).toEqual('# page1\n');
+          expect(testee.getPageText(2)).toEqual('\n# page2');
+          expect(testee.getPageText(3)).toEqual('');
+        });
+      });
+    });
+
+    describe('#getMaxPage', () => {
+      let testee: SlideService;
+      beforeEach(inject([SlideService], (s: SlideService) => testee = s));
+
+      describe('text is nothing', () => {
+        it('should be 1', () => {
+          expect(testee.getMaxPage()).toEqual(1);
+        });
+      });
+
+      describe('text has been setted', () => {
+        beforeEach(() => {
+          testee.setText('# page1\n---\n# page2');
+        });
+        it('should get last page number', () => {
+          expect(testee.getMaxPage()).toEqual(2);
+        });
+      });
+    });
+
+    describe('#getPageNo', () => {
+      let testee: SlideService;
+      beforeEach(inject([SlideService], (s: SlideService) => testee = s));
+
+      describe('text is nothing', () => {
+        it('should be 1', () => {
+          expect(testee.getPageNo(1)).toEqual(1);
+        });
+      });
+
+      describe('text has been setted', () => {
+        beforeEach(() => {
+          testee.setText('# page1\n---\n# page2');
+        });
+        it('should get page number', () => {
+          expect(testee.getPageNo(1)).toEqual(1);
+          expect(testee.getPageNo(2)).toEqual(2);
+          expect(testee.getPageNo(3)).toEqual(2);
+        });
+      });
+    });
+});
