896f5d9fb8a204f149f32d73278bf4ce8d47a8b1
joe-re <joe.tialtngo@gmail.com>
2017-03-20 20:52:50 +0900

    fix cli

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var child_process = require('child_process');
-var electron = require('electron-prebuilt');
+var electron = require('electron');
 var join = require('path').join;
 const startPath = join(__dirname, '..');
 child_process.spawn(electron, [startPath]);
